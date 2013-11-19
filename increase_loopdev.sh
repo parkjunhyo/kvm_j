9918840978274b22c93d02c49634e712d5c2c48f
root <root@ubuntu1.(none)>
2013-11-20 04:16:04 +0900

    upgrade

---
+++ increase_loopdev.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+source /root/kvm_j/kvm_setup.env
+
+## Loop devicse Increasement for the KVM generation
+## This paramter will be affected the multiple installation
+## Ubunut has default as 8, in this time, it will be changed as 128
+LOOPDEV=${LOOPDEV:=128}
+sed -i "s/\<loop\>/loot max_loop=$LOOPDEV/" /etc/modules
+modprobe loop
+let "LOOPDEV = $LOOPDEV - 1"
+for LoID in $(seq 0 $LOOPDEV)
+do
+ if [[ ! -b /dev/loop$LoID ]]
+ then
+  mknod -m0660 /dev/loop$LoID b 7 $LoID
+  chown root.disk /dev/loop$LoID
+ fi 
+done
